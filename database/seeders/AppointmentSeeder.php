7015339c3265720251c3c3e0d657ea6d41e1798c
fatimaAlbiek <albiek272@gmail.com>
2026-08-14 04:15:38 +0300

    update

--- database/seeders/AppointmentSeeder.php
+++ database/seeders/AppointmentSeeder.php
@@ -8,85 +8,85 @@ use Illuminate\Support\Facades\DB;
 
 class AppointmentSeeder extends Seeder
 {
- public function run(): void
-{
-    DB::table('appointments')->insert([
-        
-        [
-            'appointment_datetime' => '2026-06-10 09:00:00',
-            'status' => 'available',
-            'diagnosis' => null,
-            'doctor_id' => 1,
-            'patient_id' => null,
-            'created_at' => now(),
-            'updated_at' => now(),
-        ],
-        [
-            'appointment_datetime' => '2026-06-10 10:00:00',
-            'status' => 'available',
-            'diagnosis' => null,
-            'doctor_id' => 1,
-            'patient_id' => null,
-            'created_at' => now(),
-            'updated_at' => now(),
-        ],
-        [
-            'appointment_datetime' => '2026-06-10 11:00:00',
-            'status' => 'available',
-            'diagnosis' => null,
-            'doctor_id' => 1,
-            'patient_id' => null,
-            'created_at' => now(),
-            'updated_at' => now(),
-        ],
-        
-        [
-            'appointment_datetime' => '2026-06-11 09:00:00',
-            'status' => 'booked',
-            'diagnosis' => null,
-            'doctor_id' => 1,
-            'patient_id' => 1,
-            'created_at' => now(),
-            'updated_at' => now(),
-        ],
-        
-        [
-            'appointment_datetime' => '2026-06-10 09:00:00',
-            'status' => 'available',
-            'diagnosis' => null,
-            'doctor_id' => 2,
-            'patient_id' => null,
-            'created_at' => now(),
-            'updated_at' => now(),
-        ],
-        [
-            'appointment_datetime' => '2026-06-10 10:00:00',
-            'status' => 'available',
-            'diagnosis' => null,
-            'doctor_id' => 2,
-            'patient_id' => null,
-            'created_at' => now(),
-            'updated_at' => now(),
-        ],
-        
-        [
-            'appointment_datetime' => '2026-06-11 10:00:00',
-            'status' => 'booked',
-            'diagnosis' => null,
-            'doctor_id' => 2,
-            'patient_id' => 2,
-            'created_at' => now(),
-            'updated_at' => now(),
-        ],
-        [
-            'appointment_datetime' => '2026-06-12 10:00:00',
-            'status' => 'completed',
-            'diagnosis' => 'التهاب حلق',
-            'doctor_id' => 2,
-            'patient_id' => 2,
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]
-    ]);
-}  
+    public function run(): void
+    {
+        DB::table('appointments')->insert([
+
+            [
+                'appointment_datetime' => '2026-09-10 09:00:00',
+                'status' => 'available',
+                'diagnosis' => null,
+                'doctor_id' => 1,
+                'patient_id' => null,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'appointment_datetime' => '2026-09-10 10:00:00',
+                'status' => 'available',
+                'diagnosis' => null,
+                'doctor_id' => 1,
+                'patient_id' => null,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'appointment_datetime' => '2026-09-10 11:00:00',
+                'status' => 'available',
+                'diagnosis' => null,
+                'doctor_id' => 1,
+                'patient_id' => null,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+
+            [
+                'appointment_datetime' => '2026-09-11 09:00:00',
+                'status' => 'booked',
+                'diagnosis' => null,
+                'doctor_id' => 1,
+                'patient_id' => 1,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+
+            [
+                'appointment_datetime' => '2026-09-10 09:00:00',
+                'status' => 'available',
+                'diagnosis' => null,
+                'doctor_id' => 2,
+                'patient_id' => null,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'appointment_datetime' => '2026-09-10 10:00:00',
+                'status' => 'available',
+                'diagnosis' => null,
+                'doctor_id' => 2,
+                'patient_id' => null,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+
+            [
+                'appointment_datetime' => '2026-09-11 10:00:00',
+                'status' => 'booked',
+                'diagnosis' => null,
+                'doctor_id' => 2,
+                'patient_id' => 2,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'appointment_datetime' => '2026-09-12 10:00:00',
+                'status' => 'completed',
+                'diagnosis' => 'التهاب حلق',
+                'doctor_id' => 2,
+                'patient_id' => 2,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
+        ]);
+    }
 }
--- database/seeders/DepartmentSeeder.php
+++ database/seeders/DepartmentSeeder.php
@@ -8,20 +8,15 @@ use Illuminate\Support\Facades\DB;
 
 class DepartmentSeeder extends Seeder
 {
-  public function run(): void
-{
-    DB::table('departments')->insert([
-        ['name' => 'قلبية', 'description' => 'علاج أمراض القلب والشرايين', 'created_at' => now(), 'updated_at' => now()],
-        ['name' => 'عصبية', 'description' => 'علاج أمراض الجهاز العصبي', 'created_at' => now(), 'updated_at' => now()],
-        ['name' => 'عظمية', 'description' => 'علاج الكسور والمفاصل والعظام', 'created_at' => now(), 'updated_at' => now()],
-        ['name' => 'عينية', 'description' => 'علاج أمراض العيون', 'created_at' => now(), 'updated_at' => now()],
-        ['name' => 'مخبر', 'description' => 'التحاليل الطبية', 'created_at' => now(), 'updated_at' => now()],
-        ['name' => 'أشعة', 'description' => ' التصوير الشعاعي', 'created_at' => now(), 'updated_at' => now()],
-    ]);
-}  
-
+    public function run(): void
+    {
+        DB::table('departments')->insert([
+            ['name' => 'قلبية', 'description' => 'علاج أمراض القلب والشرايين', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'عصبية', 'description' => 'علاج أمراض الجهاز العصبي', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'عظمية', 'description' => 'علاج الكسور والمفاصل والعظام', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'عينية', 'description' => 'علاج أمراض العيون', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'مخبر', 'description' => 'التحاليل الطبية', 'created_at' => now(), 'updated_at' => now()],
+            ['name' => 'أشعة', 'description' => ' التصوير الشعاعي', 'created_at' => now(), 'updated_at' => now()],
+        ]);
+    }
 }
-   
-
-    
-
